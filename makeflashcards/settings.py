d1f8f8dd4a3f606f40f6dcb9c501ddf45a288c4c
hectoragvz <hectoragvz@gmail.com>
2024-06-13 14:14:14 -0700

    db

--- makeflashcards/settings.py
+++ makeflashcards/settings.py
@@ -94,12 +94,9 @@ WSGI_APPLICATION = "makeflashcards.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3","NAME": BASE_DIR / "db.sqlite3",}}
+
+DATABASES = {"default": dj_database_url.config(default=os.getenv("DATABASE_URL"))}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
